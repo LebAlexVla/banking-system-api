ea4776cecdc36848511a0a86f9d5c57be54888ff
Alexander Lebedev <lebalexvla@gmail.com>
2026-02-13 00:10:05 +0300

    feat(domain): change user session responsibility

--- src/BankingSystem.Domain/Sessions/UserSession.cs
+++ src/BankingSystem.Domain/Sessions/UserSession.cs
@@ -1,35 +1,16 @@
 using BankingSystem.Domain.BankAccounts;
-using BankingSystem.Domain.Sessions.Results;
-using BankingSystem.Domain.ValueObjects;
 
 namespace BankingSystem.Domain.Sessions;
 
 public class UserSession
 {
-    private readonly BankAccount _account;
-
-    public UserSession(Guid sessionId, BankAccount bankAccount)
+    public UserSession(Guid sessionId, BankAccountId accountId)
     {
         SessionId = sessionId;
-        _account = bankAccount;
+        AccountId = accountId;
     }
 
     public Guid SessionId { get; }
 
-    public IEnumerable<OperationType> OperationsHistory => _account.OperationsHistory;
-
-    public Balance GetBalance()
-    {
-        return _account.GetBalance();
-    }
-
-    public void Replenish(Balance balance)
-    {
-        _account.Replenish(balance);
-    }
-
-    public WithdrawMoneySessionResult WithdrawMoney(Balance balance)
-    {
-        return _account.WithdrawMoney(balance).Then();
-    }
+    public BankAccountId AccountId { get; }
 }
